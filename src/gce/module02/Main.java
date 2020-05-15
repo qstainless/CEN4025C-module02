9e0d002239b012cf6cf9244ca5be6d61924f5e28
Queue Stainless <gce517+git@gmail.com>
2020-05-14 21:37:54 -0400

    Code cleanup

--- src/gce/module02/Main.java
+++ src/gce/module02/Main.java
@@ -9,13 +9,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("MainStage.fxml"));
         primaryStage.setTitle("Castaneda - CEN4025C - Module 02");
         primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
