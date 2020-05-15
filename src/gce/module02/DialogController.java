2427557aa6bc1399d1ddd8fd5babf7e4f080cf69
Queue Stainless <gce517+git@gmail.com>
2020-05-15 09:06:06 -0400

    Updated MainDialog layout and DialogController
    Processes the data entered in the dialog and adds new to-do items to the Data model.

--- src/gce/module02/DialogController.java
+++ src/gce/module02/DialogController.java
@@ -1,9 +1,30 @@
 package gce.module02;
 
-/**
- * @author Guillermo Castaneda-Echegaray (gce517@gmail.com)
- * @since 2020-05-15
- */
+import gce.module02.model.Data;
+import gce.module02.model.Item;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
 public class DialogController {
 
+    @FXML
+    private TextField itemDescriptionField;
+
+    @FXML
+    private TextArea itemDetailsField;
+
+    @FXML
+    private DatePicker itemDueDateField;
+
+    public void processResults() {
+        String itemDescription = itemDescriptionField.getText().trim();
+        String itemDetails = itemDetailsField.getText().trim();
+        LocalDate itemDueDate = itemDueDateField.getValue();
+
+        Data.getInstance().addItem(new Item(itemDescription, itemDetails, itemDueDate));
+    }
 }
