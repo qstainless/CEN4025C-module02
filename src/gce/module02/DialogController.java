6a43789528644b937149e12e985239d6b8dc9616
Queue Stainless <gce517+git@gmail.com>
2020-05-16 11:25:11 -0400

    Added JavaDocs. Code cleanup.

--- src/gce/module02/DialogController.java
+++ src/gce/module02/DialogController.java
@@ -9,19 +9,46 @@ import javafx.scene.control.TextField;
 
 import java.time.LocalDate;
 
+/**
+ * Controller for the Add to-do item dialog
+ */
 public class DialogController {
 
+    /**
+     * The to-do item's description (title)
+     */
     @FXML
     private TextField itemDescriptionField;
 
+    /**
+     * The to-do item's details (if any)
+     */
     @FXML
     private TextArea itemDetailsField;
 
+    /**
+     * No to-do item is complete without a due date
+     */
     @FXML
     private DatePicker itemDueDateField;
 
+    /**
+     * Gets the values for the to-do item's description (title), details,
+     * and due date submitted through the Add dialog, and processes the
+     * data to add the item to the Data model.
+     *
+     * TODO:
+     * This would benefit from form validation to ensure that the user at
+     * least provides an item description. Some to-do items could be as
+     * simple as "call mom" without requiring details about what the call
+     * will entail. In other scenarios, such as "call boss" or "call client"
+     * may require additional details (e.g., an agenda for the call).
+     *
+     * @return The Item.
+     */
     public Item processResults() {
         String itemDescription = itemDescriptionField.getText().trim();
+
         /*
          The user may enter tabs in the itemDetails, which will generate
          an error when loading the items from the text file, because the
@@ -30,6 +57,7 @@ public class DialogController {
          4 spaces.
         */
         String itemDetails = itemDetailsField.getText().trim().replace("\t", "    ");
+
         LocalDate itemDueDate = itemDueDateField.getValue();
 
         // Sets the default values for empty fields
