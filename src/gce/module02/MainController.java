ef2fccabadb28f791a728c305270833942f34492
Queue Stainless <gce517+git@gmail.com>
2020-05-16 09:23:44 -0400

    Refactored.
    Move instructions out of the initialize method to their own methods. Added cellFactory controller.

--- src/gce/module02/MainController.java
+++ src/gce/module02/MainController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
@@ -26,26 +27,18 @@ public class MainController {
     @FXML
     private BorderPane mainBorderPane;
 
+    @FXML
+    private ContextMenu listContextMenu;
+
     public void initialize() {
-        // Listen to changes in the ListView to display the most recently
-        // changed item, whether it is selected programmatically or by the
-        // user, or when the user adds a new item to the to-do list
-        todoListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                Item item = todoListView.getSelectionModel().getSelectedItem();
-                dueDateLabel.setText("Due on " + item.getItemDueDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
-                itemDetailsText.setText(item.getItemDetails());
-            }
-        });
+        // Create a context menu to allow deletion of individual to-do items
+        createDeleteContextMenu();
 
-        // Populate the list view with the to-do items in the Data model
-        todoListView.setItems(Data.getInstance().getItems());
+        // Populate the ListView with the to-do items in the Data model
+        populateListView();
 
-        // Ensure that we can only select one item at a time
-        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-
-        // Select the first to-do item in the list
-        todoListView.getSelectionModel().selectFirst();
+        // Initialize cell factory for context menu functionality in the ListView
+        initializeCellFactory();
     }
 
     @FXML
@@ -89,15 +82,111 @@ public class MainController {
     }
 
     /**
-     * When an item on the ListView is selected, the item's due date and description
-     * are displayed in the TextArea
+     * Initializes the ListView's cell factory method
      */
-    @FXML
-    public void handleClickListView() {
-        Item item = todoListView.getSelectionModel().getSelectedItem();
+    public void initializeCellFactory() {
+        // We pass an anonymous class to implement the callback interface,
+        // which is part of the JavaFX API, and we pass the ListView
+        // controller and the return type (the ListCell<>)
+        todoListView.setCellFactory(new Callback<ListView<Item>, ListCell<Item>>() {
 
-        dueDateLabel.setText("Due on " + item.getItemDueDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+            @Override
+            public ListCell<Item> call(ListView<Item> param) {
+                ListCell<Item> cell = new ListCell<Item>() {
 
-        itemDetailsText.setText(item.getItemDetails());
+                    @Override
+                    protected void updateItem(Item item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setText(null);
+                        } else {
+                            setText(item.getItemDescription());
+                        }
+                    }
+                };
+
+                // We only want a context menu in non-empty cells
+                cell.emptyProperty().addListener(
+                        (obs, wasEmpty, isNowEmpty) -> {
+                            if (isNowEmpty) {
+                                cell.setContextMenu(null);
+                            } else {
+                                cell.setContextMenu(listContextMenu);
+                            }
+                        }
+                );
+
+                return cell;
+            }
+        });
+    }
+
+    /**
+     * Populates the ListView of the main stage.
+     * Listens to changes in the ListView to display the most recently
+     * changed item, whether it is selected programmatically or by the
+     * user, or when the user adds a new item to the to-do list.
+     */
+    public void populateListView() {
+        todoListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                Item item = todoListView.getSelectionModel().getSelectedItem();
+
+                String dueDate = item.getItemDueDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+
+                dueDateLabel.setText("Due on " + dueDate);
+
+                itemDetailsText.setText(item.getItemDetails());
+            }
+        });
+
+        // Populate the list view with the to-do items in the Data model
+        todoListView.setItems(Data.getInstance().getItems());
+
+        // Ensure that we can only select one item at a time
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+        // Select the first to-do item in the list
+        todoListView.getSelectionModel().selectFirst();
+    }
+
+    /**
+     * Creates the context menu to delete individual to-do items
+     */
+    public void createDeleteContextMenu() {
+        listContextMenu = new ContextMenu();
+
+        MenuItem deleteItem = new MenuItem("Delete this item");
+
+        deleteItem.setOnAction(event -> {
+            Item item = todoListView.getSelectionModel().getSelectedItem();
+            deleteItem(item);
+        });
+
+        listContextMenu.getItems().addAll(deleteItem);
+    }
+
+    /**
+     * Deletes the selected to-do item. Will show an alert and ask for
+     * confirmation before deleting the item, as the program does not
+     * offer an "undo" function.
+     *
+     * @param item The item in the Data model to be deleted
+     */
+    public void deleteItem(Item item) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+        alert.setTitle(".: WARNING :.");
+        alert.setHeaderText("You are about to delete the to-do item:\n\n" +
+                item.getItemDescription() + "\n\n" +
+                "This action cannot be undone.");
+        alert.setContentText("Click OK to confirm deletion.");
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        // Only delete de to-do item if the user clicks the OK button
+        if (result.isPresent() && (result.get() == ButtonType.OK)) {
+            Data.getInstance().deleteItem(item);
+        }
     }
 }
